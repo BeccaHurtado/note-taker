b42132d299b78edca0925aa85811d1146b9912d4
BeccaHurtado <rebeccah7.rh@gmail.com>
2022-03-13 17:59:38 -0700

    initiated data return

---
+++ server.js
@@ -0,0 +1,27 @@
+const fs = require('fs')
+const path = require('path')
+const apiRoutes = require('./routes/apiRoutes')
+const htmlRoutes = require('./routes/htmlRoutes')
+const express = require('express')
+const notes = require('./db/db.json')
+const PORT = process.env.PORT || 3000
+const app = express()
+
+
+app.use(express.static('public'))
+
+app.use(express.urlencoded({ extended: true }))
+
+app.use(express.json())
+
+app.use('./api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.get('/api/notes', (req, res) => {
+    res.send('Hello')
+})
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}`)
+})
+
