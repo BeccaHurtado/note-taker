45bc8cde31c08dadb183b91295d6e352a6c36b93
BeccaHurtado <rebeccah7.rh@gmail.com>
2022-03-13 18:06:29 -0700

    changed notes const

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path')
 const apiRoutes = require('./routes/apiRoutes')
 const htmlRoutes = require('./routes/htmlRoutes')
 const express = require('express')
-const notes = require('./db/db.json')
+const { notes } = require('./db/db.json')
 const PORT = process.env.PORT || 3000
 const app = express()
 
