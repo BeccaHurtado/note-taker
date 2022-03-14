4fdbdaf140290c9d49f4c2817a85fbffb5796657
BeccaHurtado <rebeccah7.rh@gmail.com>
2022-03-13 19:47:05 -0700

    get,post,and delete function for notes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,15 +1,46 @@
 const router = require('express').Router()
 const data = require('../db/db.json')
+const fs = require('fs')
 
 router.get('/api/notes', (req, res) => {
+    data = JSON.parse(fs.readFileSync("./db/db.json")) || []
+    console.log("GET",data)
     res.json(data)
 })
 
-router.get('/notes', (req, res) => {
-    let notes = data
-    if (notes) {
-        res.json(notes)
+router.post('/api/notes', (req, res) => {
+    let note = {
+        title: req.body.title,
+        text: req.body.text,
+        id: Math.floor(Math.random() * 1000)
     }
+    data.push(note)
+    fs.writeFileSync("./db/db.json", JSON.stringify(data), function(error){
+       if (error) throw error
+    })
+    console.log("POST",data)
+    res.json(data)
 })
+
+router.delete('/api/notes/:id', (req, res) => {
+    var updateNote = data.filter(note =>  {
+        return (note.id != req.params.id)
+    })
+    data = updateNote
+    fs.writeFileSync("./db/db.json", JSON.stringify(data), function(error){
+       if (error) throw error
+    })
+    console.log("DELETE",data)
+    res.json(data)
+})
+
+function newNote(body, notesArray) {
+    const notes = body
+    notesArray.push(notes)
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'), JSON.stringify({ notes: notesArray }, null, 2)
+    )
+    return notes
+}
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const fs = require('fs')
-const path = require('path')
+// const fs = require('fs')
+// const path = require('path')
 const apiRoutes = require('./routes/apiRoutes')
 const htmlRoutes = require('./routes/htmlRoutes')
 const express = require('express')
-const { notes } = require('./db/db.json')
+// const { notes } = require('./db/db.json')
 const PORT = process.env.PORT || 3000
 const app = express()
 
